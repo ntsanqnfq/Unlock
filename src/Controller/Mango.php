d79a1131ee24851b2fa7340356777055439b9be4
sang <sang.nguyen@nfq.asia>
2022-05-11 17:59:10 +0700

    feat: create  unit test

--- src/Controller/Mango.php
+++ src/Controller/Mango.php
@@ -5,6 +5,10 @@ class Mango extends AbstractFruit implements InterfaceTaste {
     public function taste() {
         echo "Sweet";
     }
+    public function weightScale($number, $weight) {
+        $total = $number + $weight;
+        return  $total;
+    }
 }
 
 
---
+++ tests/Controller/MangoTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace test\Controller;
+use app\Controller\Mango;
+use PHPUnit\Framework\TestCase;
+
+class MangoTest extends TestCase{
+
+}
